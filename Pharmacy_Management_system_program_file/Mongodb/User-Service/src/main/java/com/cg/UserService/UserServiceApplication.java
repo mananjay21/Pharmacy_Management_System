e62c2f0565503f4b52d5bc139e45a76293696660
Mananjay Roy <roymananjay@gmail.com>
2022-08-29 02:40:59 +0530

    doctor controller DB Updation

--- Pharmacy_Management_system_program_file/Mongodb/User-Service/src/main/java/com/cg/UserService/UserServiceApplication.java
+++ Pharmacy_Management_system_program_file/Mongodb/User-Service/src/main/java/com/cg/UserService/UserServiceApplication.java
@@ -1,5 +1,4 @@
-package com.cg.DrugsInfoService;
-
+package com.cg.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +6,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableMongoRepositories
-
-public class DrugsInfoServiceApplication {
+@EnableEurekaClient
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DrugsInfoServiceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
