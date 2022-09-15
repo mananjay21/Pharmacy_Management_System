2ce1426fa1aa6ea49d9485cd12066047754a78eb
Mananjay Roy <roymananjay@gmail.com>
2022-09-15 20:04:16 +0530

    Drugs-Info-Service Updated

---
+++ Pharmacy-Management/Drugs-Info-Service/src/main/java/com/cg/DrugsInfoService/controllers/DrugsController.java
@@ -0,0 +1,77 @@
+package com.cg.DrugsInfoService.controllers;
+
+import com.cg.DrugsInfoService.exception.ResourceNotFoundException;
+import com.cg.DrugsInfoService.models.DrugsData;
+import com.cg.DrugsInfoService.services.DrugService;
+import com.cg.DrugsInfoService.services.SequenceGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/drugs")
+@CrossOrigin
+public class DrugsController {
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
+
+    @Autowired
+    DrugService drugService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<DrugsData>> getAllDrugsData() throws ResourceNotFoundException {
+        List<DrugsData> list = drugService.getAllDrugsData();
+        if (list.isEmpty()) {
+            throw new ResourceNotFoundException("There are no medicines present in the database!");
+        }
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DrugsData> findDrugsById(@PathVariable("id") int drugId) throws ResourceNotFoundException {
+        Optional<DrugsData> drugsDataOptional = drugService.findDrugsById(drugId);
+        if (drugsDataOptional.isEmpty()) {
+            throw new ResourceNotFoundException("No medicine found with id: " + drugId);
+        }
+        return ResponseEntity.ok(drugsDataOptional.get());
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<DrugsData> saveDrugs(@RequestBody DrugsData drugsData) {
+        drugsData.setDrugId(sequenceGeneratorService.getSequenceNumber(DrugsData.SEQUENCE_NAME));
+        DrugsData savedDrugsData = drugService.saveDrugsData(drugsData);
+        return ResponseEntity.ok(savedDrugsData);
+    }
+    @GetMapping("/drugsname/{drugName}")
+    public ResponseEntity<DrugsData> getDrugsDataByDrugName(@PathVariable("drugName") String drugName) throws ResourceNotFoundException {
+        Optional<DrugsData> drugsDataOptional = drugService.getDrugsDataByDrugName(drugName);
+        if (drugsDataOptional.isEmpty()) {
+            throw new ResourceNotFoundException("No medicine found with name: " + drugName);
+        }
+        return ResponseEntity.ok(drugsDataOptional.get());
+    }
+
+    //update function
+    @PutMapping("/update/{id}")
+    public ResponseEntity<DrugsData> updateDrugsData(@RequestBody DrugsData drugsData ,
+                                                     @PathVariable("id") int drugId){
+        DrugsData updatedDrugsData = drugService.updateDrugsData(drugsData,drugId);
+        return  ResponseEntity.ok(updatedDrugsData);
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteDrugsData(@PathVariable("id") int drugId) throws ResourceNotFoundException
+    {
+        Optional<DrugsData> drugsData = drugService.findDrugsById(drugId);
+        if(drugsData.isEmpty()) {
+            throw new ResourceNotFoundException("No medicine found with id " + drugId);
+        }
+        drugService.deleteDrugsData(drugId);
+    }
+
+}
